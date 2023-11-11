cc549d86be1d41009a3a8cb613a4bcf943e7e112
Abbas Momeni <a.momeni@zoot.com>
2023-11-11 18:27:09 +0330

    add title

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+        <title>bot7.ir</title>
 
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
