f2f0f5c3b26ff62827c3f0293f6ae817c7def70e
abbas <abbasmomeny1994@gmail.com>
2024-04-15 17:17:01 +0330

    feat: add /telegram/webhook api

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,12 @@
 <?php
 
 use App\Http\Controllers\StudentController;
+use App\Http\Controllers\TelegramController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
 Route::get('/students', [StudentController::class, 'index']);
+
+Route::any('/telegram/webhook' , [TelegramController::class, 'webhook']);
