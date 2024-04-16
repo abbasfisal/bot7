d457a553f5d02d605118b52a084a8d489c7f47ff
abbas <abbasmomeny1994@gmail.com>
2024-04-16 10:07:39 +0330

    ref:webhook()

--- app/Http/Controllers/TelegramController.php
+++ app/Http/Controllers/TelegramController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Student;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Http;
 
@@ -9,9 +10,16 @@ class TelegramController extends Controller
 {
     public function webhook(Request $request)
     {
+        \Log::info('request log ', $request->all());
         $token = env('TELEGRAM_API');
         $chat_id = env('USER_ID');
-        $result = Http::get('https://api.telegram.org/bot' . $token . '/sendmessage?chat_id=' . $chat_id . '&text=hiii back ');
+        $data = Student::query()->first()->toArray();
+        var_dump($data);
+        $u = sprintf("https://api.telegram.org/bot%s/sendmessage?chat_id=%s&text=%s", $token, $chat_id, json_encode($data));
+        var_dump($u);
+
+        $result = Http::post('https://api.telegram.org/bot' . $token . '/sendmessage', ['chat_id' => $chat_id, 'text' => json_encode($data)]);
         return $result;
+
     }
 }
