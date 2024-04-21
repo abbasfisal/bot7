8b7a2ebc53df1a4179cc8c8d801391d5981909a8
abbas <abbasmomeny1994@gmail.com>
2024-04-21 15:19:10 +0330

    feat: sendMessage

--- app/Http/Controllers/TelegramController.php
+++ app/Http/Controllers/TelegramController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Http;
 use Illuminate\Support\Facades\Log;
 
 class TelegramController extends Controller
@@ -16,12 +17,17 @@ class TelegramController extends Controller
     public function webhook(Request $request)
     {
         $tData = $request->all();
+        Log::info('---- telegram incoming data ----', [$tData]);
 
-        Log::info('-- telegram data --- ', ['data' => json_encode($tData, JSON_PRETTY_PRINT)]);
+        $chatId = $tData['message']['chat'] ['id'];
+        $this->sendMessage($chatId, 'welcome to your bot ;)');
     }
 
     public function sendMessage($chatId, $text)
     {
-
+        Http::post($this->url . 'sendMessage', [
+            'chat_id' => $chatId,
+            'text'    => $text
+        ]);
     }
 }
